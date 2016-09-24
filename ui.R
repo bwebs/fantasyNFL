c1efbbdb848c3f355fa65dcb72eb1272494d608a
Bryan <bfweber@gmail.com>
2016-09-24 12:52:22 -0500

    new shiny components - WIP

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ dashboardPage(
     sidebarMenu(
       actionButton("button", "Start"),
       checkboxInput("scrapeBox", "Some valus", FALSE)
-        
+      downloadButton('downloadData', 'Download')
       )),
     dashboardBody(
       column(12, tableOutput('table')
